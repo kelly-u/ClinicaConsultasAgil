19dedf0b48cded976481525b2371e0eeec9c6abe
G. Kelly Macedo <gleicekellymacedodd@gmail.com>
2024-06-21 02:33:28 -0300

    Adição do método to String. Está cadastrando consultas, mas sem conexão com os pacientes.

--- ClinicaConsultasAgil/src/edu/gkelly/aceleradoraagil/Consulta.java
+++ ClinicaConsultasAgil/src/edu/gkelly/aceleradoraagil/Consulta.java
@@ -1,29 +1,34 @@
-package edu.gkelly.aceleradoraagil;
-
 import java.util.List;
 
 public class Consulta {
 
-    int dia;
-    String data;
+    String dia;
+    String hora;
     String especialidade;
-    List agendamentos;
 
-    // Exibir lista dos pacientes cadastrados com nº
-    // Escolhe o número, dia, hora e especialidade
-    // Adicionado à lista de agendamentos
-    public void marcarConsulta(){
-        // Só para ver no console que entrou nesse método
-        System.out.println("Marcando consulta...");
+   public Consulta(String dia, String hora, String especialidade) {
+        this.dia = dia;
+        this.hora = hora;
+        this.especialidade = especialidade;
     }
-    
-    // Mostra lista de agendamentos com nº
-    // Escolhe o nº que deseja remarcar
-    // Exibir mensagem com dia, hora e especialidade
-    // Pode cancelar a consulta ou não
-    // Cancelamento feito? Removido da lista
-    public void cancelarConsulta(){
-        // Só para ver no console que entrou nesse método
-        System.out.println("Cancelando consulta...");
-    }
+
+public String getHora() {
+       return hora;
+   }
+
+   public String getDia() {
+       return dia;
+   }
+
+   public String getEspecialidade() {
+       return especialidade;
+   }
+
+   @Override
+   public String toString() {
+       return dia;
+   }
+
 }
+
+
